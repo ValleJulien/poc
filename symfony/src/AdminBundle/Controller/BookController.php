d3a46f4a27d032a6dc71fe9d80bd6d17a7bde5c8
dev_julien <j.valle.julien@gmail.com>
2017-12-20 17:53:37 +0100

    add front method in front bundle not in admin controller

--- symfony/src/AdminBundle/Controller/BookController.php
+++ symfony/src/AdminBundle/Controller/BookController.php
@@ -11,37 +11,4 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 class BookController extends Controller
 {
 
-    /**
-     * @param int $counter
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function carListLastRegisteredBookAction(int $counter = null)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $books = $em->getRepository('AdminBundle:Book')->getLastRegisteredBook($counter);
-
-        return $this->render('AdminBundle:Books:list.html.twig', array(
-            'books' => $books
-        ));
-    }
-
-
-    /**
-     * @param Request $request
-     * @param string $slug
-     * @Route("/book-info/{slug}", name="single_book_info")
-     * @Security("is_anonymous()")
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function singleBookInfoAction(Request $request, string $slug)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $book = $em->getRepository('AdminBundle:Book')->findOneBySlug($slug);
-
-        return $this->render('AdminBundle:Books:single_book.html.twig', array(
-            'book' => $book,
-            'title' => $book->getTitle(),
-        ));
-    }
-
 }
--- symfony/src/FrontBundle/Controller/FrontController.php
+++ symfony/src/FrontBundle/Controller/FrontController.php
@@ -4,9 +4,11 @@ namespace FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class FrontController extends Controller
 {
+
     /**
      * @Route("/", name="front_homepage")
      */
@@ -18,6 +20,7 @@ class FrontController extends Controller
         ));
     }
 
+
     /**
      * @Route("/book-list", name="book_list")
      */
@@ -25,6 +28,39 @@ class FrontController extends Controller
     {
         return $this->render('FrontBundle:Pages:book_list.html.twig', array(
             'title'         => "Book List"
+        ));
+    }
+
+
+    /**
+     * @param int $counter
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function carListLastRegisteredBookAction(int $counter = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $books = $em->getRepository('AdminBundle:Book')->getLastRegisteredBook($counter);
+
+        return $this->render('FrontBundle:Pages:list.html.twig', array(
+            'books' => $books
+        ));
+    }
+
+
+    /**
+     * @param Request $request
+     * @param string $slug
+     * @Route("/book-info/{slug}", name="single_book_info")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function singleBookInfoAction(Request $request, string $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $book = $em->getRepository('AdminBundle:Book')->findOneBySlug($slug);
+
+        return $this->render('FrontBundle:Pages:single_book.html.twig', array(
+            'book' => $book,
+            'title' => $book->getTitle(),
         ));
     }
 }
